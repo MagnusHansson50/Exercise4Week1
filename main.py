753b9d37c204315f1013d5a9e1017a639a6663d1
MagnusHansson <magnus.p.hansson@gmail.com>
2025-01-12 11:52:00 +0100

    Added check for valid input

--- main.py
+++ main.py
@@ -19,9 +19,23 @@ v = input("Vilken hastighet i km/h kommer ni köra? ")
 #except ValueError:
 #    print("Det är ingen giltig hastighet")
 
-if v.isdigit():
-    v = int(v)
-    h, m, s = calculateTime(v)
-    print(f"Det kommer att ta {h} timmar {m} minuter och {s} sekunder.")
-else:
-    print("Det är ingen giltig hastighet")
+#if v.isdigit():
+#    v = int(v)
+#    h, m, s = calculateTime(v)
+#    print(f"Det kommer att ta {h} timmar {m} minuter och {s} sekunder.")
+#else:
+#    print("Det är ingen giltig hastighet")
+
+while True:
+    v = input("Vilken hastighet i km/h kommer ni köra? ")
+
+    try:
+        v = int(v)
+        if v > 0:
+            h, m, s = calculateTime(v)
+            print(f"Det kommer att ta {h} timmar {m} minuter och {s} sekunder.")
+            break
+        else:
+            print("DeLorean hastighet är inte ok, ange en positiv hastighet")
+    except ValueError:
+        print("Det är ingen giltig hastighet")
